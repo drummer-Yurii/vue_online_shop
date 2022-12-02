1fe48f058b8058d06f84507e938e94cb034e8817
Yurii Rybachok <drummer2009@ukr.net>
2022-12-02 22:16:13 +0200

    add components to project

---
+++ src/components/v-catalog-item.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="v-catalog-item">
+        <p>Item 1</p>
+        <p>Price: 100</p>
+        <button>Add to cart</button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'v-catalog-item',
+    components: {},
+    props: {},
+    data() {
+        return {
+        }
+    },
+    computed: {},
+    methods: {},
+    watch: {},
+    mounted() {}
+}
+</script>
+<style>
+       
+</style>
